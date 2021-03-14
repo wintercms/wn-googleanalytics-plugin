98030058f5a98b63a1f5e49cde44a5f0fe5a0df6
Marc Jauvin <marc.jauvin@gmail.com>
2021-03-14 14:05:49 -0400

    add class aliases

---
+++ init.php
@@ -0,0 +1,17 @@
+<?php
+
+class_alias(Winter\GoogleAnalytics\Plugin::class, RainLab\GoogleAnalytics\Plugin::class);
+
+class_alias(Winter\GoogleAnalytics\Classes\CacheItem::class, RainLab\GoogleAnalytics\Classes\CacheItem::class);
+class_alias(Winter\GoogleAnalytics\Classes\CacheItemPool::class, RainLab\GoogleAnalytics\Classes\CacheItemPool::class);
+class_alias(Winter\GoogleAnalytics\Classes\Analytics::class, RainLab\GoogleAnalytics\Classes\Analytics::class);
+
+class_alias(Winter\GoogleAnalytics\Components\Tracker::class, RainLab\GoogleAnalytics\Components\Tracker::class);
+
+class_alias(Winter\GoogleAnalytics\Models\Settings::class, RainLab\GoogleAnalytics\Models\Settings::class);
+
+class_alias(Winter\GoogleAnalytics\ReportWidgets\TrafficGoal::class, RainLab\GoogleAnalytics\ReportWidgets\TrafficGoal::class);
+class_alias(Winter\GoogleAnalytics\ReportWidgets\TopPages::class, RainLab\GoogleAnalytics\ReportWidgets\TopPages::class);
+class_alias(Winter\GoogleAnalytics\ReportWidgets\TrafficOverview::class, RainLab\GoogleAnalytics\ReportWidgets\TrafficOverview::class);
+class_alias(Winter\GoogleAnalytics\ReportWidgets\Browsers::class, RainLab\GoogleAnalytics\ReportWidgets\Browsers::class);
+class_alias(Winter\GoogleAnalytics\ReportWidgets\TrafficSources::class, RainLab\GoogleAnalytics\ReportWidgets\TrafficSources::class);
