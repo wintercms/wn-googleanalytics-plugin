270e2963a3ff69249382b71e5105ba578a48743e
Luke Towers <github@luketowers.ca>
2023-01-24 13:55:26 -0600

    Add support for disabling the tracker when no tracking ID is set, a backend user is making the request, or the site is in maintenance mode.
    Replaces #6

--- components/Tracker.php
+++ components/Tracker.php
@@ -1,16 +1,63 @@
 <?php namespace Winter\GoogleAnalytics\Components;
 
+use BackendAuth;
+use Config;
 use Cms\Classes\ComponentBase;
+use Cms\Models\MaintenanceSetting;
 use Winter\GoogleAnalytics\Models\Settings;
 
 class Tracker extends ComponentBase
 {
+    /**
+     * @var bool Flag for the current state of the tracker component
+     */
+    public $enabled = true;
+
     public function componentDetails()
     {
         return [
             'name'        => 'winter.googleanalytics::lang.strings.tracker',
             'description' => 'winter.googleanalytics::lang.strings.tracker_desc'
         ];
+    }
+
+    public function onRun(): void
+    {
+        // Disable the tracker when no tracking ID is set
+        if (empty($this->trackingId())) {
+            $this->enabled = false;
+        }
+
+        // Disable the tracker when authenticated backend users are detected
+        if (
+            !Config::get('winter.googleanalytics::track_backend_users', false)
+            && BackendAuth::getUser()
+        ) {
+            $this->enabled = false;
+        }
+
+        // Disable the tracker when in maintenance mode
+        if (
+            !Config::get('winter.googleanalytics::track_maintenance_mode', false)
+            && $this->isMaintenanceModeEnabled()
+            // && (
+            //     app()->maintenanceMode()->active()
+            //     || $this->isMaintenanceModeEnabled()
+            // )
+        ) {
+            $this->enabled = false;
+        }
+    }
+
+    /**
+     * isMaintenanceModeEnabled will check if maintenance mode is currently enabled.
+     * Static page logic should be disabled when this occurs.
+     */
+    protected function isMaintenanceModeEnabled(): bool
+    {
+        return MaintenanceSetting::isConfigured() &&
+            MaintenanceSetting::get('is_enabled', false) &&
+            !BackendAuth::getUser();
     }
 
     public function trackingId()
